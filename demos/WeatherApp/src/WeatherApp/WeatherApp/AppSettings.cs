a32f0b53a6ba34a8952d07a7cb391c2320c4f36c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-09-30 21:09:10 +0200

    Added samples

---
+++ demos/WeatherApp/src/WeatherApp/WeatherApp/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public class AppSettings
+    {
+        public static string ApiKey = "INSERT YOUR API KEY HERE";
+        public static bool IsImperial = false;
+    }
+}
